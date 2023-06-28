ff76881d53829b4f9b6f21d641790cd76deb5942
yuvictoria0412 <yuvictoria0412@gmail.com>
2023-06-28 22:30:20 +0800

    still have bug... ha...

--- Pocket_algorithm.py
+++ Pocket_algorithm.py
@@ -1,11 +1,10 @@
 import requests
 import io
 import numpy as np
-from unittest import result
 import matplotlib.pyplot as plt
 
 # training data
-traindata_url = "https://www.csie.ntu.edu.tw/~htlin/mooc/datasets/mlfound_math/hw1_18_train.dat" #input("please enter file url: ")
+traindata_url = "https://www.csie.ntu.edu.tw/~htlin/mooc/datasets/mlfound_math/hw1_18_train.dat"
 traindata = requests.get(traindata_url)
 traindata.raise_for_status() # check http status
 traindata = np.loadtxt(io.BytesIO(traindata.content), encoding = 'bytes')
@@ -15,35 +14,33 @@ testdata_url = "https://www.csie.ntu.edu.tw/~htlin/mooc/datasets/mlfound_math/hw
 testdata = requests.get(testdata_url)
 testdata.raise_for_status()
 testdata = np.loadtxt(io.BytesIO(testdata.content), encoding = 'bytes')
-# print(testdata.size)
 
 
 def evaluate(best_w, w, data):
+
     count = error(w, data)
-    
     if best_w[1] > count:
-        print("ori best", best_w)
         best_w = w, count
-        print("new best", best_w)
     return best_w
 
 def error(w, data):
-    count = 0
+
+    error_count = 0
     for x in data:
         y = int(x[4])
         x = np.append([1], x[0:4])
 
         if int(np.sign(w.dot(x))) == 0:
             if y != -1:             # set sign(0) = -1
-                count += 1
+                error_count += 1
         elif int(np.sign(w.dot(x))) != y:
-            count += 1
-    return count
+            error_count += 1
+    return error_count/data.size
 
 def pocket_algor(n, data, coeff):
-    # Pocket Algorithm
+
     w = np.zeros(5)
-    best_w = w, data.size
+    best_w = np.zeros(5), data.size
     cnt = 0
 
     while cnt < n:
@@ -53,12 +50,12 @@ def pocket_algor(n, data, coeff):
 
             if int(np.sign(w.dot(x))) == 0:
                 if y != -1:
-                    w += coeff * y * x
+                    w = w + coeff * y * x
                     cnt += 1
                     best_w = evaluate(best_w, w, data)
 
             elif int(np.sign(w.dot(x))) != y:
-                w += coeff * y * x
+                w = w + coeff * y * x
                 cnt += 1
                 best_w = evaluate(best_w, w, data)
 
@@ -70,7 +67,7 @@ def pocket_algor(n, data, coeff):
 
 
 def PLA(n, data, coeff):
-    # Perceptron Learning Algorithm
+    '''n represents iteration time, PLA with coefficient coeff'''
     w = np.zeros(5)
     cnt = 0
     
@@ -81,11 +78,11 @@ def PLA(n, data, coeff):
 
             if int(np.sign(w.dot(x))) == 0:
                 if y != -1:
-                    w += coeff * y * x
+                    w = w + coeff * y * x
                     cnt += 1
 
             elif int(np.sign(w.dot(x))) != y:
-                w += coeff * y * x
+                w = w + coeff * y * x
                 cnt += 1
 
             if cnt == n:
@@ -95,34 +92,34 @@ def PLA(n, data, coeff):
 
 # Q18: pocket algorithm iteration 50 repeat 2000 #
 error_rate = 0
-n = 100
+n = 2000
+
 for i in range(n):
     w_pocket = pocket_algor(50, traindata, 1)
-    # print(w_pocket)
-    error_rate += (error(w_pocket, testdata)/testdata.size)
-    # print(error_rate/(i+1))
+    error_rate += error(w_pocket, testdata)
     np.random.shuffle(traindata)  
+
 error_rate = error_rate/n
 print(error_rate)
 
 # Q19: PLA algorithm iteration 50 repeat 2000 #
 error_rate = 0
-# n = 10
+
 for i in range(n):
     w_pocket = PLA(50, traindata, 1)
-    # print(w_pocket)
-    error_rate += error(w_pocket, testdata)/testdata.size
-    # print(error_rate/(i+1))
-    np.random.shuffle(traindata)  
+    error_rate += error(w_pocket, testdata)
+    np.random.shuffle(traindata) 
+
 error_rate = error_rate/n
 print(error_rate)
 
 # Q20: pocket algorithm iteration 100 repeat 2000 #
-# error_rate = 0
-# n = 2000
-# for i in range(n):
-#     w_pocket = pocket_algor(100, traindata, 1)
-#     error_rate += error(w_pocket, testdata)/testdata.size
-#     np.random.shuffle(traindata)
-# error_rate = error_rate/n
-# print(error_rate)
+error_rate = 0
+
+for i in range(n):
+    w_pocket = pocket_algor(100, traindata, 1)
+    error_rate += error(w_pocket, testdata)/testdata.size
+    np.random.shuffle(traindata)
+
+error_rate = error_rate/n
+print(error_rate)
